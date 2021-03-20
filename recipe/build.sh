737890237be59c77e2444d0f3639df195b2e8d19
Isuru Fernando <isuruf@gmail.com>
2021-03-20 03:07:03 -0500

    use gnuconfig

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.guess config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.sub config.sub
 
 export CXXFLAGS="-g $CXXFLAGS"
 
